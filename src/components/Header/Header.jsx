986d7669b52bd2e0f19b95fcaa277d9ba002c702
Ram Sah <mohan2036@gmail.com>
2023-09-27 10:05:49 -0400

    added header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+
+
 import "./Header.scss";
 
 const Header = () => {
-    return <div>Header</div>;
+    return <header className="main-header">
+        <div className="header-content">
+            {/*  ul>li*3  shortcut for list*/}
+            <ul className="left">
+                <li>Home</li>
+                <li>About</li>
+                <li>Categories</li>
+            </ul>
+            <div className="center">OURSTORE</div>
+            <div className="right">
+                <TbSearch />
+                <AiOutlineHeart />
+                <span className="cart-icon">
+                    <CgShoppingCart />
+                    <span>5</span>
+                </span>
+            </div>
+        </div>
+    </header>
 };
 
 export default Header;
